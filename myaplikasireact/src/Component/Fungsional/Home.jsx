e6e67a9b09e494d2142a04769cb1c48316fbe6a6
firdaayunirmala <ayunirmalafirda@gmail.com>
2020-06-16 20:39:28 +0700

    belajar props

--- myaplikasireact/src/Component/Fungsional/Home.jsx
+++ myaplikasireact/src/Component/Fungsional/Home.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import Blog from './Blog'
 
 const Home = () => {
-    return <div><h1>Ini adalah Halaman Utama</h1></div>
+    return <div>
+        <Blog
+            tanggal="15 juni 2020"
+            judul=" Teknologi Blockchain"
+            summary=" Generate Lorem Ipsum placeholder text for use in your graphic"
+        />
+        <Blog
+            tanggal="16 juni 2020"
+            judul=" Teknologi Internet of Things"
+            summary=" Generate Lorem Ipsum placeholder text for use in your graphic"
+        />
+        <Blog
+            tanggal="17 juni 2020"
+            judul=" Teknologi Blockchain"
+            summary=" Generate Lorem Ipsum placeholder text for use in your graphic"
+        />
+    </div>
 }
 
 export default Home;
