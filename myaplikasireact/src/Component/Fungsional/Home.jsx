6edd6df9fd05ac41b61f2ac897df11121bc18276
firdaayunirmala <ayunirmalafirda@gmail.com>
2020-06-17 12:25:30 +0700

    belajar membuat state untuk button beli

--- myaplikasireact/src/Component/Fungsional/Home.jsx
+++ myaplikasireact/src/Component/Fungsional/Home.jsx
@@ -1,23 +1,15 @@
 import React from 'react'
-import Blog from './Blog'
+//import Blog from './Blog'
+import Produk from '../Class/Produk';
 
 const Home = () => {
     return <div>
-        <Blog
-            tanggal="15 juni 2020"
-            judul=" Teknologi Blockchain"
-            summary=" Generate Lorem Ipsum placeholder text for use in your graphic"
-        />
-        <Blog
-            tanggal="16 juni 2020"
-            judul=" Teknologi Internet of Things"
-            summary=" Generate Lorem Ipsum placeholder text for use in your graphic"
-        />
-        <Blog
-            tanggal="17 juni 2020"
-            judul=" Teknologi Blockchain"
-            summary=" Generate Lorem Ipsum placeholder text for use in your graphic"
-        />
+    <Produk nama="Xiaomi Pro 8" stock="10" harga="3.000.000"/>
+    <Produk nama="Xiaomi Pro 8" stock="10" harga="3.000.000"/>
+    <Produk nama="Xiaomi Pro 8" stock="10" harga="3.000.000"/>
+    <Produk nama="Xiaomi Pro 8" stock="10" harga="3.000.000"/>
+    <Produk nama="Xiaomi Pro 8" stock="10" harga="3.000.000"/>
+    <Produk nama="Xiaomi Pro 8" stock="10" harga="3.000.000"/>
     </div>
 }
 
